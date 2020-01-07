4030d5bfa2b6e2cfb012f978bfe145656c88555b
Kong Chae Won <chaewonkong@gmail.com>
2020-01-07 19:45:47 +0900

    feat: initial commit

---
+++ Yetda_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yetda_iOS
+//
+//  Created by Leon Kong on 2020/01/07.
+//  Copyright © 2020 Yetda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
