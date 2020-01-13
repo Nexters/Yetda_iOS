2acf9db83d00a1dc3a47b1d39ce231e3113f94d3
Kong Chae Won <chaewonkong@gmail.com>
2020-01-13 20:00:01 +0900

    #8 create ViewControllers using Cocoa Touch

--- Yetda_iOS/ResultView/ResultViewController.swift
+++ Yetda_iOS/ResultView/ResultViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2020 Yetda. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
