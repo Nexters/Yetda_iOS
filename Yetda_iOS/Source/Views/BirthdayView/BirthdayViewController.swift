efad1f11216875c2796eb9d2dd9825e70fc2f80e
Kong Chae Won <chaewonkong@gmail.com>
2020-01-22 18:46:20 +0900

    #24 Custom 월/일 UIPickerView 작성

--- Yetda_iOS/Source/Views/BirthdayView/BirthdayViewController.swift
+++ Yetda_iOS/Source/Views/BirthdayView/BirthdayViewController.swift
@@ -6,18 +6,77 @@
 //  Copyright © 2020 Yetda. All rights reserved.
 //
 
+// Ref: https://github.com/bendodson/MonthYearPickerView-Swift/blob/master/MonthYearPickerView.swift
+
 import UIKit
 import SnapKit
 
-class BirthdayViewController: UIViewController {
+class BirthdayViewController: BaseViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var months: [String]! = []
+    var days: [String]! = []
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return months.count
+        case 1:
+            return days.count
+        default:
+            return 0
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return months[row]
+        case 1:
+            return days[row]
+        default:
+            return nil
+        }
+    }
+    
+    func populateData() {
+        for month in 1...12 {
+            months.append(String("\(month)월"))
+        }
+        
+        for day in 1...31 {
+            days.append(String("\(day)일"))
+        }
+    }
+    
+    
+    
+    var datePicker: UIPickerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
 
+        populateData()
+        setDatePicker()
         // Do any additional setup after loading the view.
     }
     
+    func setDatePicker() {
+        datePicker = UIPickerView()
+        
+        view.addSubview(datePicker)
+        datePicker.delegate = self
+        datePicker.dataSource = self
+        
+        datePicker.snp.makeConstraints { (make) in
+            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(20)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(-20)
+        }
+    }
 
     /*
     // MARK: - Navigation
