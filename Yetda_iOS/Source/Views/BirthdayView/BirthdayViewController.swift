09c3aaec39928e7718c3cfa7f91f37dc844f9b39
Kong Chae Won <chaewonkong@gmail.com>
2020-01-24 17:24:17 +0900

    #24 생일 와이어프레임 화면 리팩토링 진행
    BirthdayViewController-layout.swift 파일을 생성하여 UI 관련 로직 이동.

--- Yetda_iOS/Source/Views/BirthdayView/BirthdayViewController.swift
+++ Yetda_iOS/Source/Views/BirthdayView/BirthdayViewController.swift
@@ -15,46 +15,6 @@ class BirthdayViewController: BaseViewController, UIPickerViewDelegate, UIPicker
     
     var months: [String]! = []
     var days: [String]! = []
-    
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch component {
-        case 0:
-            return months.count
-        case 1:
-            return days.count
-        default:
-            return 0
-        }
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        switch component {
-        case 0:
-            return months[row]
-        case 1:
-            return days[row]
-        default:
-            return nil
-        }
-    }
-    
-    func populateData() {
-        for month in 1...12 {
-            months.append(String("\(month)월"))
-        }
-        
-        for day in 1...31 {
-            days.append(String("\(day)일"))
-        }
-    }
-    
-    
-    
     var datePicker: UIPickerView!
 
     override func viewDidLoad() {
@@ -63,19 +23,6 @@ class BirthdayViewController: BaseViewController, UIPickerViewDelegate, UIPicker
         populateData()
         setDatePicker()
         // Do any additional setup after loading the view.
-    }
-    
-    func setDatePicker() {
-        datePicker = UIPickerView()
-        
-        view.addSubview(datePicker)
-        datePicker.delegate = self
-        datePicker.dataSource = self
-        
-        datePicker.snp.makeConstraints { (make) in
-            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(20)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(-20)
-        }
     }
 
     /*
