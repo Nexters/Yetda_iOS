23993644f001a2ff3d12f1439ad69c3dfcb09119
Kong Chae Won <chaewonkong@gmail.com>
2020-01-27 17:42:26 +0900

    #28 GenderViewController 생성 및 Storyboard 연결

--- Yetda_iOS/Source/Views/GenderView/GenderViewController.swift
+++ Yetda_iOS/Source/Views/GenderView/GenderViewController.swift
@@ -8,3 +8,38 @@
 
 import UIKit
 import SnapKit
+
+class GenderViewController: BaseViewController {
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           // Do any additional setup after loading the view.
+           navigationController?.setNavigationBarHidden(false, animated: true)
+       }
+       
+       // custom setup
+       override func setup() {
+           super.setup()
+       }
+       
+       // MARK: - apply layout from extension
+       override func setupUI() {
+           super.setupUI()
+           
+//           setButton()
+//           setVerticalStackView()
+//           setGuideLabel()
+//           setNameTextField()
+           
+       }
+       
+       override func setupButton(button: UIButton) {
+           super.setupButton(button: nextButton)
+           button.setTitle("다음", for: .normal)
+       }
+    
+    
+}
