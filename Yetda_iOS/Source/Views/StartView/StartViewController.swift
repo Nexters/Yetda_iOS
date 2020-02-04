b5c0baff030c471ce6b0444fbd12a5e295c406f7
Kong Chae Won <chaewonkong@gmail.com>
2020-02-04 18:00:54 +0900

    #31 Realm에 데이터를 저장하는 프로세스를 ResultsViewController로 이동.
    Realm에 데이터를 Write하는 프로세스를 메서드로 독립.

--- Yetda_iOS/Source/Views/StartView/StartViewController.swift
+++ Yetda_iOS/Source/Views/StartView/StartViewController.swift
@@ -45,47 +45,6 @@ class StartViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let presents = Presents()
-//        presents.name = "향수"
-//        presents.tags.append("뷰티")
-//        presents.tags.append("실용")
-        
-        let results = Results()
-        
-        let gift1 = Gift()
-        gift1.name = "향수"
-        gift1.tags.append("뷰티")
-        gift1.tags.append("실용")
-        
-        let gift2 = Gift()
-        gift2.name = "꽃다발"
-        gift2.tags.append("여성")
-        gift2.tags.append("감성")
-        
-        let receiver = Receiver()
-        receiver.name = "May"
-        receiver.gifts.append(gift1)
-        receiver.gifts.append(gift2)
-        
-        results.receivers.append(receiver)
-        
-        do {
-            let realm = try Realm()
-            try realm.write {
-                realm.add(results)
-            }
-            let outcome = realm.objects(Results.self)
-            print(outcome[0].receivers)
-            
-        } catch let error as NSError {
-            // handle error
-            print(error)
-        }
-        
-//        try! realm.write{
-//            realm.add(presents)
-//        }
-        
         
         //        let settings = FirestoreSettings()
         //        Firestore.firestore().settings = settings
