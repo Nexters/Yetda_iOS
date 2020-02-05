83df9b056fca5642f4cf5a3a04e6028cc7b68000
Kong Chae Won <chaewonkong@gmail.com>
2020-02-06 01:17:08 +0900

    #43 PageViewController가 적용된 test 브랜치를 최신 브랜치에 머지함

--- UIColor+.swift
+++ UIColor+.swift
@@ -16,4 +16,8 @@ extension UIColor {
     static let shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.03)
     static let veryLightPink = UIColor(red: 201/255, green: 201/255, blue: 201/255, alpha: 1)
     static let veryLightPink2 = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+    
+    static func blueGreyWith(alpha: CGFloat) -> UIColor {
+        return UIColor(red: 140/255, green: 144/255, blue: 152/255, alpha: alpha)
+    }
 }
