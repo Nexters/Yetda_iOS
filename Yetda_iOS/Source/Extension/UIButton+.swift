95d0d69ff2835d3242ec34390395ccca49735aa4
Kong Chae Won <chaewonkong@gmail.com>
2020-02-05 16:05:20 +0900

    #41 브랜치 머지 과정에서 발생한 conflict 해결

--- Yetda_iOS/Source/Extension/UIButton+.swift
+++ Yetda_iOS/Source/Extension/UIButton+.swift
@@ -18,6 +18,30 @@ extension UIButton {
         setNextButtonEnable(isEnabled)
     }
     
+    
+    func setYesNoButton(isYesBtn: Bool) {
+
+        if isYesBtn {
+            self.setTitle("O", for: .normal)
+            self.setTitleColor(UIColor.pastelRed, for: .normal)
+        } else {
+            self.setTitle("X", for: .normal)
+            self.setTitleColor(UIColor.blueGrey, for: .normal)
+        }
+
+        self.titleLabel?.font = .boldSystemFont(ofSize: 28)
+        self.layer.cornerRadius = 38
+        self.layer.borderColor = UIColor.paleLilac.cgColor
+        self.layer.borderWidth = 2
+    }
+    
+    func setUnderLine() {
+        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue]
+        let underlineAttributedString = NSAttributedString(string: "StringWithUnderLine", attributes: underlineAttribute)
+        self.titleLabel?.attributedText = underlineAttributedString
+
+    }
+    
     func setNextButtonEnable(_ enable: Bool){
         if enable {
             self.setTitleColor(.white, for: .normal)
