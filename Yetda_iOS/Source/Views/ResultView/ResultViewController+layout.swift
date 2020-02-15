f9a0212832e71bd71875eb8263f9a01cd2035eda
Kong Chae Won <chaewonkong@gmail.com>
2020-02-16 03:30:15 +0900

    #68 add dumb button to ResultView

---
+++ Yetda_iOS/Source/Views/ResultView/ResultViewController+layout.swift
@@ -0,0 +1,14 @@
+//
+//  ResultViewController+layout.swift
+//  Yetda_iOS
+//
+//  Created by Leon Kong on 2020/02/16.
+//  Copyright © 2020 Yetda. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+extension ResultViewController {
+    
+}
--- Yetda_iOS/Source/Views/ResultView/ResultViewController.swift
+++ Yetda_iOS/Source/Views/ResultView/ResultViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import RealmSwift
 
 class ResultViewController: BaseViewController {
+    var backButton =  UIButton()
+    var cardView = UIView()
     
     static func instance(viewModel: HomeViewModel) -> ResultViewController? {
         let resultViewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "ResultViewController") as? ResultViewController
@@ -18,20 +20,39 @@ class ResultViewController: BaseViewController {
     }
     
     fileprivate var homeViewModel: HomeViewModel?
+    
+    override func setup() {
+        setButtonUI()
+        backButton.addTarget(self, action: #selector(nextBtnTapped), for: .touchUpInside)
+    }
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
 
         // Do any additional setup after loading the view.
     }
+    
+    func setButtonUI() {
+        backButton.setNextButton(isEnable: true, title: "처음으로 돌아가기")
+        self.view.addSubview(backButton)
+        
+        // setup self view contraints
+        backButton.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.left.right.equalTo(self.view).inset(24)
+            make.bottom.equalTo(self.view).inset(50)
+            make.height.equalTo(44)
+        }
+    }
+
         
 }
 
 extension ResultViewController: HomeViewControllerable {
     func next() {
-        homeViewModel?.startBtnTapped()
+//        homeViewModel?.startBtnTapped()
+        print("처음으로 돌아가기")
     }
     
     func prev() {
